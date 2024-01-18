e4fc556ce8121b0a3a2651a1be1445dd0c1bba6c
Gui-Alucard <guilhermeanv7822@gmail.com>
2024-01-18 18:37:27 -0300

    :bug: Lint-staged
    fixing config to fix with eslint

--- src/components/AnimatedPhoto/index.tsx
+++ src/components/AnimatedPhoto/index.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import mainPic from "@/assets/main-picture.jpg";
+import Image from 'next/image'
+import mainPic from '@/assets/main-picture.jpg'
 
 export default function AnimatedPhoto() {
   return (
@@ -11,13 +11,13 @@ export default function AnimatedPhoto() {
           src={mainPic}
           alt="picture of me"
           style={{
-            width: "80%",
-            height: "auto",
+            width: '80%',
+            height: 'auto',
           }}
           priority={false}
           className="w-fit animate-mainPhoto border border-purple-logo bg-cover bg-center drop-shadow-main-photo transition-all delay-150 ease-in-out md:min-w-72 md:max-w-[480px]"
         />
       </div>
     </div>
-  );
+  )
 }
